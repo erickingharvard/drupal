b740642fb9cbbb526dd27cb1910eb20f3b26b30e
erickingharvard <eric_king@harvard.edu>
2015-08-17 16:05:38 -0400

    Update install_new_relic.sh

--- scripts/install_new_relic.sh
+++ scripts/install_new_relic.sh
@@ -4,5 +4,5 @@ rpm -Uvh http://yum.newrelic.com/pub/newrelic/el5/x86_64/newrelic-repo-5-3.noarc
 yum install -y newrelic-php5 || true
 
 export NR_INSTALL_SILENT=true
-export NR_INSTALL_KEY=your_new_relic_key_here
+export NR_INSTALL_KEY=ca6f6450d39120b0ab5aed8dcf52404365d395d2
 newrelic-install install
